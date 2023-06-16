63774730a3449a1ac87ee320785386e6b5d24e8a
Pierre Pocreau <pierre.pocreau@gmail.com>
2023-06-16 15:49:42 +0200

    comment and simplification of dual.m

--- dual.m
+++ dual.m
@@ -1,27 +1,22 @@
-bits = 4;
-dim_H = bits + 1; % We don't consider qubits, but more general system. Furthermore, |0...0> will not query the Oracle.
+% Script to compute the dual SDP formulated in the paper,
+% for the Boolean function of id 5865
+% of truth table: "0001011011101001" 
+% and polynomial representation: x1 + x3x4 + x2x3 + x2x4 + x2x3x4
+
+bits = 4; % Number of bits of the Boolean function considered
+dim_H = bits + 1; % The oracles takes a register of dimension 5
 T=2;
-symmetries=0;
-
-%Function id 6630: "0001100111100110": x3x4 + x2 + x2x4 + x2x3 + x2x3x4 + x1
-%Should be in the same NPN class as: 
-%x1x2x4 + x1 + x3 + x4 : 0110011010011100 (the initial function we looked at)
-%f = boolean([0 1 1 0 0 1 1 0 1 0 0 1 1 1 0 0]);
-
-%Function id 5865: "0001011011101001" x1 + x3x4 + x2x3 + x2x4 + x2x3x4
-%(simplest NPN equivalent function with largest gap)
-f = boolean([0 0 0 1 0 1 1 0 1 1 1 0 1 0 0 1]);
-
-
 settings = sdpsettings('showprogress',1,'savesolverinput',1,'savesolveroutput',1,'dualize',0,'solver','scs','scs.eps',1e-6,'scs.eps_abs',1e-6,'scs.eps_rel', 0, 'scs.max_iters',50000,'dimacs',1);
 
-%Generate variables
+f = boolean([0 0 0 1 0 1 1 0 1 1 1 0 1 0 0 1]); % Loads the function
+
+% Generate the dual variables
 dim = dim_H^(2*T);
 W = sdpvar(dim,dim,'symmetric');
 lambda{1} = sdpvar(8,1);
 lambda{2} = sdpvar(8,1);
 
-%Process constraints
+% Generate the dual cone contraints and the constraints on the lambdas.
 dim = size(W,1);
 dim_H = exp(log(dim)/(2*T));
 
@@ -38,9 +33,10 @@ A{T+2}{2} = [];
 
 constr_QCFO_dual = in_QCFO_dual_cone(W, d, A);
 constr_GEN_dual = in_valid_dual_cone(W, d, A); 
-
 constr_lambda = [sum(lambda{1}) + sum(lambda{2}) <= 1, lambda{1} >= 0, lambda{2} >= 0];
 
+
+% Generate the constraints linked to the oracles
 oracles = oracles_map(dim_H, bits, T);
 
 constr_0 = 0;
@@ -64,20 +60,22 @@ constr1 =  W - constr_1 >= 0;
 constr_GEN_dual = [constr_GEN_dual, constr_lambda, constr1, constr0];
 constr_QCFO_dual = [constr_QCFO_dual, constr_lambda, constr1, constr0];
 
-obj = 1 + trace(W)/25 - sum(lambda{1}) - sum(lambda{2}); 
-%%
-%Optimisation
+% Dual objectif
+obj = 1 + trace(W)/dim_H^2 - sum(lambda{1}) - sum(lambda{2}); 
+
+% Optimisation
 optout_gen = optimize(constr_GEN_dual, obj, settings);
 W_gen_dual = value(W);
 lambda_gen{1} = value(lambda{1});
 lambda_gen{2} = value(lambda{2});
-gen_dual = - (trace(value(W_gen_dual))/25 - sum(lambda_gen{1}) - sum(lambda_gen{2}));
+% Extract the value of epsilon from the objectif (1 - epsilon)
+gen_dual = - (trace(value(W_gen_dual))/dim_H^2 - sum(lambda_gen{1}) - sum(lambda_gen{2}));
 
 optout_fo = optimize(constr_QCFO_dual, obj, settings);
 W_fo_dual = value(W);
 lambda_fo{1} = value(lambda{1});
 lambda_fo{2} = value(lambda{2});
-fo_dual = - (trace(value(W_fo_dual))/25 - sum(lambda_fo{1}) - sum(lambda_fo{2}));
+% Extract the value of epsilon from the objectif (1 - epsilon)
+fo_dual = - (trace(value(W_fo_dual))/dim_H^2 - sum(lambda_fo{1}) - sum(lambda_fo{2}));
 
-%Results
 [fo_dual, gen_dual]
