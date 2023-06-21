81c0f2d047d9f4ef7076bf994dc6b162847a32c1
Pierre Pocreau <pierre.pocreau@gmail.com>
2023-06-21 12:52:05 +0200

    commentary on the trucating script

--- truncating.m
+++ truncating.m
@@ -1,28 +1,33 @@
-%%
-bits = 4;
-dim_H = bits + 1; % We don't consider qubits, but more general system. Furthermore, |0...0> will not query the Oracle.
-T = 2;
-symmetries = 0;
-symbolic = false;
+% Script to extract rigorous lower and upper bounds from the dual and
+% primal solutions of the SDP. We adapt a method developped in:
+% Strict Hierarchy between Parallel, Sequential, and
+% Indefinite-Causal-Order Strategies for Channel Discrimination, Bavaresco,
+% Murao, Quintino.
 
-%Function id 5865: "0001011011101001" x1 + x3x4 + x2x3 + x2x4 + x2x3x4
-%(simplest NPN equivalent function with largest gap)
+% This script computes bounds for the following function:
+% id 5865
+% truth table: "0001011011101001" 
+% polynomial representation: x1 + x3x4 + x2x3 + x2x4 + x2x3x4
+bits = 4; % Number of bits of the Boolean function considered
+dim_H = bits + 1; % The oracles takes a register of dimension 5
+T=2;
+symbolic = true; % To avoid numerical imprecision we use the symbolic representation.
+
 func = boolean([0 0 0 1 0 1 1 0 1 1 1 0 1 0 0 1]);
-%func = boolean([0 1 1 0 0 1 1 0 1 0 0 1 1 1 0 0]), %other f  with gap
 oracles = oracles_map(dim_H, bits, T);
-%%
+
+% Loads the solutions obtained from solving the primal and dual sdp
 load("primal_5865.mat")
 load("dual_5865.mat")
-%load("primal_f.mat")
-%load("dual_f.mat")
 
-
-%%
-% Truncation of primal lower bound
+% Truncation of the primal solutions. It gives a lower bound on the
+% objectif 1-epsilon, so an upper bound on epsilon.
 p_fo = [];
 p_gen = [];
 W_gen_trunc = trunc_lower(W_Gen, T, 5, symbolic);
 W_fo_trunc = trunc_lower(W_FO, T, 2, symbolic);
+
+% Compute the value of the lower bound.
 for x = dec2bin(0:2^bits-1)' - '0'
     im = func(x');
     Ox = oracles(num2str(x'));
@@ -30,18 +35,17 @@ for x = dec2bin(0:2^bits-1)' - '0'
     p_gen = [p_gen, trace(W_gen_trunc{im+1}*transpose(Ox))];
 end
 
-%%
+% Truncation of the dual solution. It gives an upper bound on the objectif
+% 1-epsilon, so a lower bound on epsilon.
 [W_gen_dual_trunc, lambda_gen_trunc] = trunc_upper(W_gen_dual, lambda_gen, bits, func, T, 5, symbolic);
 [W_fo_dual_trunc, lambda_fo_trunc] = trunc_upper(W_fo_dual, lambda_fo, bits, func, T, 2, symbolic);
 
-%%
-%Bound on epsilon on both cases:
+% Compute the value of the two bounds on epsilon.
 primal_gen = 1-min(p_gen);
 dual_gen = - (trace(W_gen_dual_trunc)/25 - sum(lambda_gen{1}) - sum(lambda_gen{2}));
 
 primal_fo = 1-min(p_fo);
 dual_fo = - (trace(W_fo_dual)/25 - sum(lambda_fo{1}) - sum(lambda_fo{2}));
 
-%Bounds
 bounds_gen = [dual_gen, primal_gen]
 bounds_fo = [dual_fo, primal_fo]
