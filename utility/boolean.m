583a4ee811f127fb60ca6b0fd4879f17a48f9305
Pierre Pocreau <pierre.pocreau@gmail.com>
2023-06-21 13:30:54 +0200

    simplification of the oracle creation, and commentaries on the boolean file

--- utility/boolean.m
+++ utility/boolean.m
@@ -1,13 +1,14 @@
-function handle = boolean(output)
-    handle = @(x) eval(output, x);
+function handle = boolean(tt)
+    % Return the function define by the truth table tt.
+    handle = @(x) eval(tt, x);
 end
 
-function y = eval(output, x)
-    % A function is defined by its 16 bits output vector.
+function y = eval(tt, x)
+    % Evaluate the function defined by its truth table tt, on input x.
     if size(output, 2) ~=  2^size(x, 2)
-        bits_to_cut =  log(2^size(x, 2) / size(output,2))/log(2);
+        bits_to_cut =  log(2^size(x, 2) / size(tt,2))/log(2);
         x = x(1:end-bits_to_cut);
     end
     index = bin2dec(num2str(x))+1;
-    y = output(index);
+    y = tt(index);
 end
---
+++ utility/oracles_map.m
@@ -0,0 +1,33 @@
+function oracles = oracles_map(n_bits, T)
+% Generate a dictionnary mapping for each bit vector x, its
+% corresponding query oracle in its Choi form.
+% The query oracles are tensored T times, corresponding to the number
+% of query allowed for the computation (up to T=3).
+
+oracles = containers.Map;
+inputs = dec2bin(0:2^n_bits-1) - '0'; %Generate all possile inputs
+
+for x = inputs'
+    Ox = pure_CJ(query_oracle_x(x));
+    Ox = Ox*Ox';
+
+    if T == 2
+        Ox = kron(Ox, Ox);
+    end
+    if T == 3
+        Ox = kron(Ox, kron(Ox, Ox));
+    end
+
+    oracles(num2str(x')) = Ox;
+end
+end
+
+function oracle = query_oracle_x(x)
+% Generate the oracle of a bit string x.
+    bits = length(x);
+    oracle = zeros(bits);
+    oracle(1, 1) = 1;
+    for i = 1:bits
+        oracle(i+1, i+1) = (-1)^x(i);
+    end
+end
