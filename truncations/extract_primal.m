d1389891c66d03cf0f099ce9ab2a049eb235c127
Pierre Pocreau <pierre.pocreau@gmail.com>
2023-10-10 15:08:40 +0200

    title modification and comments in extract_primal

--- truncations/extract_primal.m
+++ truncations/extract_primal.m
@@ -1,7 +1,8 @@
-function W_final = trunc_primal(W, T, supermapClass, symbolic)
-%Modifies the process W obtained after SDP optimisation so that it 
-%verifies the constraints exactly. It will provide a lower bound of the
-%objective (1-epsilon) and therefore an upper bound on epsilon.
+function W_final = extract_primal(W, T, supermapClass, symbolic)
+% Extract an exact primal solution from a numerical approximation. 
+% This function gives a lower bound on the objective (1-epsilon) and therefore an upper bound on epsilon.
+% It follows Algorithm 2 as described in the paper:
+% A. A. Abbott, M. Mhalla, P. Pocreau, "Quantum Query Complexity of Boolean Functions under Indefinite Causal Order", arXiv:2307.10285 
 
     dim = size(W{1}, 1);
     dim_H = exp(log(dim)/(2*T));
@@ -9,7 +10,7 @@ function W_final = trunc_primal(W, T, supermapClass, symbolic)
     d = dim_H * ones(1,2*T);
     d = [1 d 1];
     A{1}{1} = 1;
-    A{1}{2} = [];
+    A{1}{2} = []; % trivial P
     for i = 1:T
         A{i+1}{1} = 2*i;
         A{i+1}{2} = 2*i + 1;
@@ -17,23 +18,23 @@ function W_final = trunc_primal(W, T, supermapClass, symbolic)
     A{T+2}{1} =  2*T+2;
     A{T+2}{2} = [];
     
-    %This rationalization procedure cleans the matrix.
+    % Rationalise each element of the superinstrument W
     [N, D] = rat(W{1});
     W_truncated{1} = N./D;
     [N, D] = rat(W{2});
     W_truncated{2} = N./D;
     
-    %Use a symbolic representation.
+    % Use a symbolic representation
     if symbolic == true
         W_truncated{1} = sym(W_truncated{1});
         W_truncated{2} = sym(W_truncated{2});
     end
     
-    %Assure that the process is self-adjoint.
+    % Ensure that the superinstrument elements are self-adjoints
     W_autoAdj{1} = (W_truncated{1} + W_truncated{1}') / 2;
     W_autoAdj{2} = (W_truncated{2} + W_truncated{2}') / 2;
 
-    %Assure that we have a valid process, by projecting it on the space.
+    % Project W onto the space of valid QCFO or general supermaps
     switch supermapClass
     case 2 % QC-FO
         W_valid = project_onto_QCFOs(W_autoAdj{1} + W_autoAdj{2}, d, A);
@@ -41,13 +42,17 @@ function W_final = trunc_primal(W, T, supermapClass, symbolic)
         W_valid = project_onto_valid_superops(W_autoAdj{1} + W_autoAdj{2}, d, A);
     end
     
+    % Ensure that W is in the valid space of supermaps by removing the orthogonal part 
     W_rest = W_valid - W_autoAdj{1} - W_autoAdj{2};
     W_ok{1} = W_autoAdj{1} + W_rest / 2;
     W_ok{2} = W_autoAdj{2} + W_rest / 2;
     
-    %The matrices must be positive
-    %Computing the eigenvalues in symbolic mode takes too much time, so we
-    %computes them in numeric and shifts them a little bit.
+    % Ensure that the superinstrument elements are positive semi-definite
+    % by mixing them with the identity.
+    % This step can be performed analytically, however computing the
+    % eigenvalues in symbolic mode can be computationally inefficient.
+    % Here we compute the eigenvalue numerically and shift them by a small
+    % constant.
     if symbolic == true
         W_double{1} = double(W_ok{1});
         W_double{2} = double(W_ok{2});
@@ -63,20 +68,21 @@ function W_final = trunc_primal(W, T, supermapClass, symbolic)
         W_ok{2} = (1-eps)*W_ok{2} + eps*eye(dim);
     end
 
-    %Renormalize
+    % Renormalise the process matrix
     norm = trace(W_ok{1} + W_ok{2});
     W_final{1} = dim_H^T/norm * W_ok{1};
     W_final{2} = dim_H^T/norm * W_ok{2};
 
-    % Check the process is indeed in the right space -> that projection is idempotent
-    switch supermapClass
-        case 2 % QC-FO
-            assert(10^-8 >= max(max(abs(W_final{1} + W_final{2} - project_onto_QCFOs(W_final{1} + W_final{2}, d, A)))));
-        otherwise % General supermaps
-            assert(10^-8 >=  max(max(abs(W_final{1} + W_final{2} - project_onto_valid_superops(W_final{1} + W_final{2}, d, A)))));
-    end
+    % Check that all the constraints are verified
     [~, flag1] = chol(W_final{1});
     [~, flag2] = chol(W_final{2});
     assert(flag1==0);
-    assert(flag2==0);
+    assert(flag2==0);    
+    switch supermapClass
+        case 2 % QC-FO
+            assert(0 == max(max(abs(W_final{1} + W_final{2} - project_onto_QCFOs(W_final{1} + W_final{2}, d, A)))));
+        otherwise % General supermaps
+            assert(0 ==  max(max(abs(W_final{1} + W_final{2} - project_onto_valid_superops(W_final{1} + W_final{2}, d, A)))));
+    end
+
 end
