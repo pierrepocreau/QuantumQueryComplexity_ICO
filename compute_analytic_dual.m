a31c702837eb93aadb01a6066a419b3c3d9e595a
Pierre Pocreau <pierre.pocreau@gmail.com>
2023-10-17 10:51:52 +0200

    typo and simpler constraints for dual

--- compute_analytic_dual.m
+++ compute_analytic_dual.m
@@ -9,7 +9,6 @@ T=2;
 settings = sdpsettings('showprogress',1,'savesolverinput',1,'savesolveroutput',1,'dualize',0,'solver','scs','scs.eps',1e-6,'scs.eps_abs',1e-6,'scs.eps_rel', 0, 'scs.max_iters',50000,'dimacs',1);
 
 f = boolean_function_from_table([0 0 0 1 0 1 1 0 1 1 1 0 1 0 0 1]); % Loads the function
-
 % Generate the dual variables and their constraints
 d = dim_H * ones(1,2*T);
 d = [1 d 1];
@@ -27,9 +26,11 @@ for i = 1:T
 end
 spaces{T+2}{1} =  2*T+2;
 spaces{T+2}{2} = [];
-
-constr_QCFO_dual = in_QCFO_dual_cone(W, d, spaces);
-constr_GEN_dual = in_valid_dual_cone(W, d, spaces); 
+%%
+Wproj_QCFO = project_onto_QCFOs(W - trace(W)/dim * eye(dim), d, spaces);
+constr_QCFO_dual = Wproj_QCFO == 0;
+Wproj_gen = project_onto_valid_superops(W - trace(W)/dim * eye(dim), d, spaces);
+constr_GEN_dual = Wproj_gen == 0;
 
 constr_lambda = [sum(lambda{1}) + sum(lambda{2}) <= 1, lambda{1} >= 0, lambda{2} >= 0];
 
@@ -58,7 +59,7 @@ constr_GEN_dual = [constr_GEN_dual, constr_lambda, constr1, constr0];
 constr_QCFO_dual = [constr_QCFO_dual, constr_lambda, constr1, constr0];
 
 % Dual objective
-obj = 1 + trace(W)/dim_H^2 - sum(lambda{1}) - sum(lambda{2}); 
+obj = 1 + trace(W)/dim_H^T - sum(lambda{1}) - sum(lambda{2}); 
 
 % Optimisation
 optout_gen = optimize(constr_GEN_dual, obj, settings);
@@ -66,13 +67,13 @@ W_gen_dual = value(W);
 lambda_gen{1} = value(lambda{1});
 lambda_gen{2} = value(lambda{2});
 % Extract the value of epsilon from the objective (1 - epsilon)
-gen_dual = - (trace(value(W_gen_dual))/dim_H^2 - sum(lambda_gen{1}) - sum(lambda_gen{2}));
+gen_dual = - (trace(value(W_gen_dual))/dim_H^T - sum(lambda_gen{1}) - sum(lambda_gen{2}));
 
 optout_fo = optimize(constr_QCFO_dual, obj, settings);
 W_fo_dual = value(W);
 lambda_fo{1} = value(lambda{1});
 lambda_fo{2} = value(lambda{2});
 % Extract the value of epsilon from the objective (1 - epsilon)
-fo_dual = - (trace(value(W_fo_dual))/dim_H^2 - sum(lambda_fo{1}) - sum(lambda_fo{2}));
+fo_dual = - (trace(value(W_fo_dual))/dim_H^T - sum(lambda_fo{1}) - sum(lambda_fo{2}));
 
 [fo_dual, gen_dual]
