30adfadf3725d6f33b36a6a09da0af353ad6c9f4
Pierre Pocreau <pierre.pocreau@gmail.com>
2023-10-18 10:09:56 +0200

    simplification of the projectors on duals spaces

--- utility/project_onto_dual_QCFO_superops.m
+++ utility/project_onto_dual_QCFO_superops.m
@@ -1,38 +1,8 @@
 function W_proj = project_onto_dual_QCFO_superops(W, dims_raw, parties_raw)
-    %% Setup and process the input
+%project_onto_dual_QCFO_superops projects a superoperator onto the subspace
+%of unnormalised process in the dual space of the QCFOs.
+%This projector can be written from the projector on the space of QCFOs.
 
-    % First put W in canonical ordering (this checks the input validity too)
-    % The spaces P,AI,AO,...,F then correspond to dims 1,2,3,...,2*N+2
-    if exist('parties_raw','var') && ~isempty(parties_raw)
-        [W, dims, parties] = superop_to_canonical_ordering(W, dims_raw, parties_raw);
-    else
-        [W, dims, parties] = superop_to_canonical_ordering(W, dims_raw);
-        parties_raw = parties;
-    end
+    W_proj = W - project_onto_QCFOs(W, dims_raw, parties_raw) + tr_replace(W, 1:length(dims_raw), dims_raw);
 
-    N = length(parties) - 2;
-    
-    %% Implement projection separately and explicitly for each N
-    switch N
-        case 2
-            %%
-            %Assumes dim P and F are 1 ?
-            P = 1;
-            AI = 2;
-            AO = 3;
-            A = [AI, AO];
-            BI = 4;
-            BO = 5;
-            B = [BI, BO];
-            F = 6;
-
-            W_proj = W - (tr_replace(W, BO, dims) - tr_replace(W, B, dims));
-            W_proj = W_proj - (tr_replace(W_proj, [AO, B], dims) - tr_replace(W_proj, [A, B], dims));
-
-        otherwise
-            error('Projector onto valid superoperators currently only implemented up to N=2.');
-    end
-
-    % Put W back in its original ordering
-    W_proj = superop_from_canonical_ordering(W_proj,dims_raw,parties_raw);
 end
